90b493c24efed96b94a2b4d474554d6ecf700d28
Christopher Denny <chris.denny@utexas.edu>
2016-01-16 17:38:32 -0600

    fixed decimal precision

--- db/migrate/20160116230605_foreign_keys.rb
+++ db/migrate/20160116230605_foreign_keys.rb
@@ -1,9 +1,9 @@
 class ForeignKeys < ActiveRecord::Migration
   def change
-	add_foreign_key :people, :teams, :primary_key
-	add_foreign_key :contacts, :teams
-	add_foreign_key :contacts, :people
-	add_foreign_key :people_events, :people
-  	add_foreign_key :people_events, :events
+	add_foreign_key :people, :teams, primary_key: "id"
+	add_foreign_key :contacts, :teams, primary_key: "id"
+	add_foreign_key :contacts, :people, primary_key: "id"
+	add_foreign_key :people_events, :people, primary_key: "id"
+  	add_foreign_key :people_events, :events, primary_key: "id"
   end
 end
