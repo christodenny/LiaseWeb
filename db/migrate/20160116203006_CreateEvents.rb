2076d5003d3cbb876f38bd8f8bdb0bf5d012d674
Christopher Denny <chris.denny@utexas.edu>
2016-01-16 15:15:52 -0600

    fixed decimal precision

---
+++ db/migrate/20160116203006_CreateEvents.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+  	create_table :events do |t|
+		t.timestamp :time
+		t.integer :length
+		t.string :place
+		t.string :name
+		t.decimal :longitude, :precision => 9, :scale => 6
+		t.decimal :latitude, :precision => 9, :scale => 6
+	end
+  end
+end
