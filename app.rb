71274d8a6d0a84f6bffd617bc426bfe82a0d0f81
Christopher Denny <chris.denny@utexas.edu>
2016-01-16 21:38:12 -0600

    event creator done

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'json'
-
 db = URI.parse('postgres://oaafdlhwmkznte:mP2aHxLDrp7Xz_5Hy7KQo_hpBC@ec2-54-225-165-132.compute-1.amazonaws.com:5432/d58tgju84l2ipv')
-
 ActiveRecord::Base.establish_connection(
 	:adapter => 'postgresql',
 	:host => db.host,
@@ -16,23 +14,18 @@ ActiveRecord::Base.establish_connection(
 class Event < ActiveRecord::Base
 	self.table_name = "events"
 end
-
 class People_Event < ActiveRecord::Base
 	self.table_name = "people_events"
 end
-
 class Person < ActiveRecord::Base
 	self.table_name = "people"
 end
-
 class Team < ActiveRecord::Base
 	self.table_name = "teams"
 end
-
 class Contact < ActiveRecord::Base
 	self.table_name = "contacts"
 end
-
 
 get "/" do
 	"hello world"
@@ -55,6 +48,17 @@ get "/events" do
 	events = Event.all.to_json
 end
 
+get "/eventbyid" do
+	event = Event.find_by(id: params[:id])
+end
+
+get "/eventsbypersonid" do
+	array = []
+	pplevents = People_Event.where(ppl_id: params[:id])
+	pplevents.each { |pplevent| array += Event.where(id: pplevent.event_id)}
+	ret = array.to_json
+end
+
 post "/newpplevent" do
 	People_Event.create(ppl_id: params[:ppl_id], event_id: params[:event_id])
 	"new ppl event saved"
@@ -62,6 +66,10 @@ end
 
 get "/pplevents" do
 	pplevents = People_Event.all.to_json
+end
+
+get "ppleventbyid" do
+	pplevent = People_Event.find_by(id: params[:id])
 end
 
 post "/newteam" do
@@ -73,6 +81,10 @@ get "/teams" do
 	teams = Team.all.to_json
 end
 
+get "/teambyid" do
+	team = Team.find_by(id: params[:id])
+end
+
 post "/newperson" do
 	Person.create(name: params[:name], team_id: params[:team])
 	"new person saved"
@@ -80,6 +92,14 @@ end
 
 get "/people" do
 	people = Person.all.to_json
+end
+
+get "/personbyid" do
+	person = Person.find_by(id: params[:id])
+end
+
+get "/peoplebyteamid" do
+	people = Person.where(team_id: params[:id]).to_json
 end
 
 post "/newcontact" do
@@ -94,5 +114,13 @@ end
 
 get "/contacts" do
 	contacts = Contact.all.to_json
+end
+
+get "/contactbyid" do
+	contact = Contact.find_by(id: params[:id])
+end
+
+get "/contactsbyteamid" do
+	contacts = Contact.where(team_id: params[:id]).to_json
 end
 
