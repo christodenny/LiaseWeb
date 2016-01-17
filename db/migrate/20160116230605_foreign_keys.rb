3d89d2cd1d6228346ba848ed99024c95b4aa9517
Christopher Denny <chris.denny@utexas.edu>
2016-01-16 20:30:17 -0600

    event creator done

--- db/migrate/20160116230605_foreign_keys.rb
+++ db/migrate/20160116230605_foreign_keys.rb
@@ -1,9 +1,9 @@
 class ForeignKeys < ActiveRecord::Migration
   def change
-	add_foreign_key :people, :teams, column: :id, name: :team_id
-	add_foreign_key :contacts, :teams, column: :id, name: :team_id
-	add_foreign_key :contacts, :people, column: :id, name: :ppl_id
-	add_foreign_key :people_events, :people, column: :id, name: :ppl_id
-  	add_foreign_key :people_events, :events, column: :id, name: :event_id
+	add_foreign_key :people, :teams, column: :team_id, name: :fk_people_teams
+	add_foreign_key :contacts, :teams, column: :team_id, name: :fk_contacts_teams
+	add_foreign_key :contacts, :people, column: :ppl_id, name: :fk_contacts_people
+	add_foreign_key :people_events, :people, column: :ppl_id, name: :fk_ppl_events_people
+  	add_foreign_key :people_events, :events, column: :event_id, name: :fk_ppl_events_events
   end
 end
