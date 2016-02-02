11d74f4076da282c2ed8fa827f71f79498d188f7
christodenny <chris.denny@utexas.edu>
2016-02-01 18:31:10 -0600

    trying more efficient update

--- app.rb
+++ app.rb
@@ -69,11 +69,10 @@ put "/events/:id" do
 	e.description = params.has_key?(:description) ? params[:description] : e.description
 	e.save
 =end
-	params.each do |key, val|
+	params.keys.each do |key|
 		#if e.has_attribute?(key)
 			#e.update(name: "#{val}")
 		puts key
-		puts val
 	status 202
 end
 
